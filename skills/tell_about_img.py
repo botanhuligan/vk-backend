ce1917c93b7d5e5e215089f329fc1f045dea08dd
Vitaliy Rakitin <vitaliyrakitin@ya.ru>
2019-09-29 10:49:30 +0300

    base scenarios

--- skills/tell_about_img.py
+++ skills/tell_about_img.py
@@ -4,16 +4,41 @@ import requests
 
 from engine import Skill, Action, Answer
 
-where_is_skill = Skill("static/tell_about_img.yaml")
+tell_about_img = Skill("static/tell_about_img.yaml")
 
 
 # -------- Custom Actions --------
 class TellAbout(Action):
     def run(self, message, user_id, context: Dict):
         resp = requests.get("http://classifier:8585/get_document/" + str(message))
-        print(resp)
-        return Answer("ща всё будет")
+        context["doc"] = resp.json()
+        if context["doc"]:
+            return Answer(
+                "Картина {1} автора {0}. Рассказать подробнее?".format(context["doc"]["author"], context["doc"]["name"]),
+                "{2} - безумно интересное прозиведение. Автор картины {0}. {1} Рассказать подробнее?".format(
+                    context["doc"]["author"], context["doc"]["description"], context["doc"]["name"]
+                ),
+                url_img=context["doc"]["img"]
+            )
+        else:
+            self.event("RESTART", user_id, message)
+            return Answer("К сожалению, я не знаю такого произведения. Попробуй ещё." )
+
+
+class TellMoreAbout(Action):
+    def run(self, message, user_id, context: Dict):
+        if context["doc"]:
+            return Answer(
+                "Картина {1} автора {0}.".format(context["doc"]["author"], context["doc"]["name"]),
+                "Запускаю",
+                url_img=context["doc"]["img"],
+                url_voice=context["doc"]["audio"]
+            )
+        else:
+            self.event("RESTART", user_id, message)
+            return Answer("К сожалению я потерял нить разговора. Попробуй ещё.")
 
 
 # -------- Register Actions --------
-where_is_skill.register_action("tell_about_obj", TellAbout)
+tell_about_img.register_action("tell_about_obj", TellAbout)
+tell_about_img.register_action("tell_more_about_obj", TellMoreAbout)
